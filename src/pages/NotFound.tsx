c4cdebfedc83c7f9cd0719035822972a84f56fe3
Atul <atulshrma91@gmail.com>
2025-10-16 15:15:01 +0530

    dependencies - latest version upgrades

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,18 +1,7 @@
-import { useLocation } from "react-router-dom";
-import { useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import Logo from "@/components/Logo";
 
 const NotFound = () => {
-  const location = useLocation();
-
-  useEffect(() => {
-    console.error(
-      "404 Error: User attempted to access non-existent route:",
-      location.pathname
-    );
-  }, [location.pathname]);
-
   return (
     <div className="min-h-screen flex items-center justify-center px-4" style={{ 
       borderTop: '3px solid black',
